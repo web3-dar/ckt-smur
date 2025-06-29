b92c4531f8262c4cec4ca7f5ba3afd67b08d9b31
Pawacode Database <whakhydoh@gmail.com>
2025-06-29 15:31:21 +0100

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,36 @@
+
+import Footer from '../Home/footer';
+import Header from '../Home/header';
+
+import HelpSection from '../Home/hero2';
+import hero from '../assets/hero.jpg';
+import Promotions from '../components/promotions';
+import RatesSection from '../components/ratesection';
+
+
+const Home: React.FC = () => {
+ 
+
+  return (
+    <>
+
+    <Header/>
+
+    
+   
+    <HelpSection/>
+    
+    <div className="w-full flex justify-center">
+  <img src={hero} alt="Hero" className="max-w-full h-auto" />
+</div>
+
+    <Promotions/>
+    <RatesSection/>
+    <Footer/>
+    
+    </>
+  
+  );
+};
+
+export default Home;
